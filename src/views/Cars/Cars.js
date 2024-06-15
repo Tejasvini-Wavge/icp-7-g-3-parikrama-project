8c630e993c71c52a3c66d90b221c465df1807d86
Vedantdhobl18 <vedantdhoble192@gmail.com>
2024-06-15 14:46:57 +0530

    added some basic code

---
+++ src/views/Cars/Cars.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+
+function Cars() {
+  return (
+    <div>
+      <h1>cars</h1>
+    </div>
+  )
+}
+
+export default Cars
