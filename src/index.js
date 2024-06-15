9603df171c585e6c9c3ae199c570373f10aa31e1
codewithtarde9370 <teja36491@gmail.com>
2024-06-15 11:53:28 +0530

    issues resolved

--- src/index.js
+++ src/index.js
@@ -2,17 +2,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Register from './views/Register/Register';
-<<<<<<< HEAD
 import Hotels from './views/Hotels/Hotels';
-=======
 import Home from './views/Stays/Stays';
-<
 
-=======
->>>>>>> 23c8795373d4ab245d4eeadeb813e27622ee5338
->>>>>>> d42a0291f6cc728939f9252066adcb9249b282c0
-
-git 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
