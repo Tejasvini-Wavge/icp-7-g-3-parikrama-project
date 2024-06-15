d3edb4f9047e0ab4e5603deb6231a9df9c71b293
codewithtarde9370 <teja36491@gmail.com>
2024-06-15 10:07:32 +0530

    added changes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Register from './views/Register/Register';
+import Home from './views/Stays/Stays'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 const router = createBrowserRouter([
+    
+    {
+        path: "/",
+        element:<Home/>
+    },
+  //   {
+  //     path: "/flights",
+  //     element:<Flights/>
+  // }
+//   {
+//     path: "/hotels",
+//     element:<Hotels/>
+// },
+// {
+//   path: "/rental",
+//   element:<Rental/>
+// },
+// {
+//   path: "/cars",
+//   element:<Cars/>
+// },
+//   path: "/register",
+//   element:<Register/>
     {
         path: "/",
         element: <Register/>
---
+++ src/views/Stays/Stays.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from '../../components/navbar/navbar';
+import Footer from '../../components/footer/footer';
+import'./stays.css';
+
+function Stays() {
+  return (
+    <>
+    <Navbar className="navbar" />
+    <div  className='background'>
+      
+    </div>
+    <Footer className="footer"/>
+
+    </>
+  )
+}
+
+export default Stays
