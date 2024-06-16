3d93bc91c3724527af14cdb3f6f4719ea76937de
vishnu192003 <chacherkarvishnu@gmail.com>
2024-06-16 11:33:43 +0530

    f221

--- src/views/Rental/Rental.js
+++ src/views/Rental/Rental.js
@@ -6,108 +6,29 @@ import butterfly from './Rental.css';
 import image1 from './images/img-1.jpg';
 import image2 from './images/img2.jpg';
 import image3 from './images/img-3.jpg';
+import image4 from './images/img-5.jpg';
 
-
-  
-
+import citilinkLogo from './images/flt-3.webp';
+import goibiboLogo from './images/goibibo.webp';
+import airIndiaLogo from './images/flt-2.webp';
+import malaysiaAirlinesLogo from './images/flt-1.webp';
 function Rental() {
-  const properties = [
-    {
-      type: 'House',
-      image: image1,
-      title: 'Apogee Property Advisors',
-      features: 
-      {
-        beds: 3,
-        baths: 2,
-        sqft: 2100,
-        store: 1,
-      },
-      price: 492,
-      rating: 2.6,
-      reviews: 142,
-    },
-    {
-      type: 'Villa',
-      image: image2,
-      title: 'Haven Group Real Estate',
-      features: 
-      {
-        beds: 3,
-        baths: 2,
-        sqft: 2100,
-        store: 1,
-      },
-      price: 492,
-      rating: 4.6,
-      reviews: 142,
-    },
-   
-    {
-      type: 'Apartment',
-      image: image3,
-      title: 'Larkspur Partners Realty',
-      features: {
-        beds: 3,
-        baths: 2,
-        sqft: 2100,
-        store: 1,
-      },
-      price: 492,
-      rating: 4.6,
-      reviews: 142,
-    },
-  
-
-
  
-  ];
   return (
-
     <>
     <Navbar className="navbar" /> 
 
-    <div  className='background-rental'>
+    <div  className='background-rental'></div>
       
-    </div>
+    
 
-    <div className="app">
-      <h1>Explore Hot Rental Properties</h1>
-      <div className="properties">
-        {properties.map((property, index) => (
-          <div className="property" key={index}>
-            <img src={property.image} alt={property.title} />
-            <div className="property-info">
-              <div className="type">{property.type}</div>
-              <h2>{property.title}</h2>
-              <div className="features">
-                <div>
-                  <span className='border'>{property.features.beds} Beds</span>
-                  <span className='border'>{property.features.baths} Baths</span>
-                  <span className='border'>{property.features.sqft} sqft</span>
-                  <span className='border'>{property.features.store} Store</span>
-                </div>
-              </div>
-              <div className="price">
-                <span className="discount">15% Off</span>
-                <span>From US${property.price}</span>
-              </div>
-              <div className="rating">
-                <span className="stars">★★★★★</span>
-                <span className="review-count">{property.rating} ({property.reviews} Reviews)</span>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-      <button className="more">More</button>
-    </div>
+    
 
     
     <div className="container">
       <div className="card">
         <div className="card-image">
-          <img src="citilink-logo.png" alt="Citilink Logo" />
+          <img src={citilinkLogo} alt="Citilink Logo" />
         </div>
         <div className="card-content">
           <h3>Flat</h3>
@@ -118,7 +39,7 @@ function Rental() {
       </div>
       <div className="card">
         <div className="card-image">
-          <img src="goibibo-logo.png" alt="Goibibo Logo" />
+          <img src={goibiboLogo} alt="Goibibo Logo" />
         </div>
         <div className="card-content">
           <h3>Flat</h3>
@@ -129,7 +50,7 @@ function Rental() {
       </div>
       <div className="card">
         <div className="card-image">
-          <img src="" alt="Air India Logo" />
+          <img src={airIndiaLogo} alt="Air India Logo" />
         </div>
         <div className="card-content">
           <h3>Flat</h3>
@@ -140,7 +61,7 @@ function Rental() {
       </div>
       <div className="card">
         <div className="card-image">
-          <img src="malaysia-airlines-logo.png" alt="Malaysia Airlines Logo" />
+          <img src={malaysiaAirlinesLogo} alt="Malaysia Airlines Logo" />
         </div>
         <div className="card-content">
           <h3>Flat</h3>
@@ -148,6 +69,112 @@ function Rental() {
           <p>On Domestic Flights</p>
           <button className="button">LOG125F</button>
         </div>
+      </div>
+    </div>
+
+    <div className="app">
+      <div className="container1">
+        <h1>Explore Hot Rental Properties</h1>
+        <div className="properties">
+          <div className="property">
+            <div className="image">
+              <img src={image1} alt="House" />
+            </div>
+            <div className="info">
+              <h2>House</h2>
+              <h3>Apogee Property Advisors</h3>
+              <div className="details">
+                <div>3 Beds</div>
+                <div>2 Baths</div>
+                <div>2100 sqft</div>
+                <div>1 Store</div>
+              </div>
+              <div className="price">
+                <span className="discount1">15% Off</span>
+                <span className="amount">From US$492</span>
+              </div>
+              <div className="rating">
+                <span>★★★★★</span>
+                <span>4.6 (142 Reviews)</span>
+              </div>
+            </div>
+          </div>
+          <div className="property">
+            <div className="image">
+              <img src={image2} alt="Villa" />
+            </div>
+            <div className="info">
+              <h2>Villa</h2>
+              <h3>Haven Group Real Estate</h3>
+              <div className="details">
+                <div>3 Beds</div>
+                <div>2 Baths</div>
+                <div>2100 sqft</div>
+                <div>1 Store</div>
+              </div>
+              <div className="price">
+                <span className="discount1">15% Off</span>
+                <span className="amount">From US$492</span>
+              </div>
+              <div className="rating">
+                <span>★★★★★</span>
+                <span>4.6 (142 Reviews)</span>
+              </div>
+            </div>
+          </div>
+          <div className="property">
+            <div className="image">
+              <img src={image3} alt="Apartment" />
+            </div>
+            <div className="info">
+              <h2>Apartment</h2>
+              <h3>Larkspur Partners Realty</h3>
+              <div className="details">
+                <div>3 Beds</div>
+                <div>2 Baths</div>
+                <div>2100 sqft</div>
+                <div>1 Store</div>
+              </div>
+              <div className="price">
+                <span className="discount1">15% Off</span>
+                <span className="amount">From US$492</span>
+              </div>
+              <div className="rating">
+                <span>★★★★★</span>
+                <span>4.6 (142 Reviews)</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="property">
+            <div className="image">
+              <img src={image4} alt="Apartment" />
+            </div>
+            <div className="info">
+              <h2>Apartment</h2>
+              <h3>Larkspur Partners Realty</h3>
+              <div className="details">
+                <div>3 Beds</div>
+                <div>2 Baths</div>
+                <div>2100 sqft</div>
+                <div>1 Store</div>
+              </div>
+              <div className="price">
+                <span className="discount1">15% Off</span>
+                <span className="amount">From US$492</span>
+              </div>
+              <div className="rating">
+                <span>★★★★★</span>
+                <span>4.6 (142 Reviews)</span>
+              </div>
+            </div>
+          </div>
+
+         
+
+          
+        </div>
+        <button className="more">More</button>
       </div>
     </div>
 
