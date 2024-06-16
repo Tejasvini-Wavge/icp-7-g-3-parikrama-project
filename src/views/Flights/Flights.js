e5bb5897e06eab6d34f7437241ca4349a87a3989
shriyaborkar358 <shriyaborkar71@gmail.com>
2024-06-16 17:24:32 +0530

    header image added

--- src/views/Flights/Flights.js
+++ src/views/Flights/Flights.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import './Flights.css'
+import HeaderImg from './images/header-flight-img.jpg'
 import Navbar from '../../components/Navbar/Navbar'
 
 function Flights() {
   return (
     <>
     <Navbar/>
+    <img src={HeaderImg} alt={HeaderImg} className="header-img" />
     </>
   )
 }
