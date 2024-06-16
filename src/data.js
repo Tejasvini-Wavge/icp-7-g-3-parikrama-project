4c8b37fede9100dec9c2f997f791e64624fcbfcd
codewithtarde9370 <teja36491@gmail.com>
2024-06-16 18:15:45 +0530

    added cards of offer

--- src/data.js
+++ src/data.js
@@ -1,32 +1,39 @@
+import img1 from './img/data-img/offer1.jpg';
+import img2 from "./img/data-img/offer2.jpg";
+import img3 from "./img/data-img/offer3.jpg";
+import img4 from "./img/data-img/offer4.jpg";
+import img5 from "./img/data-img/offer5.jpg";
+import img6 from "./img/data-img/offer6.jpg";
+
 const travelOffers = [
     {
-      img: 'https://example.com/destination1.jpg',
-      offerText: 'Explore the beaches of Bali with 20% off on all resorts!',
+      img: img1,
+      offerText: 'Explore beaches with 20% off on resorts!',
       validity: 'Valid until June 30, 2024'
     },
     {
-      img: 'https://example.com/destination2.jpg',
-      offerText: 'Discover the magic of Paris - Book now and save 15%',
+      img: img2,
+      offerText: 'Discover the magic- Book now and save 15%',
       validity: 'Valid until July 15, 2024'
     },
     {
-      img: 'https://example.com/destination3.jpg',
-      offerText: 'Adventure awaits in New Zealand - 25% off on guided tours',
+      img: img3,
+      offerText: 'Adventure awaits- 25% off on guided tours',
       validity: 'Valid until August 1, 2024'
     },
     {
-      img: 'https://example.com/destination4.jpg',
-      offerText: 'Experience the wonders of Japan - 10% off on all travel packages',
+      img: img4,
+      offerText: 'Experience- 10% off on packages',
       validity: 'Valid until September 10, 2024'
     },
     {
-      img: 'https://example.com/destination5.jpg',
-      offerText: 'Summer special: 30% off on all Caribbean cruises!',
+      img:img5,
+      offerText: 'Summer special: 30% off!',
       validity: 'Valid until July 31, 2024'
     },
     {
-      img: 'https://example.com/destination6.jpg',
-      offerText: 'Discover the beauty of Iceland - Save 20% on all bookings',
+      img: img6,
+      offerText: 'Discover- Save 20% on  bookings',
       validity: 'Valid until October 5, 2024'
     }
   ];
