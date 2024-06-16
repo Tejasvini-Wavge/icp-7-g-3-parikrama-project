9fc125b8da5b490ced089f0af655c7833c6bf17a
codewithtarde9370 <teja36491@gmail.com>
2024-06-16 18:01:43 +0530

    added cards of offer

--- src/views/Stays/Stays.js
+++ src/views/Stays/Stays.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
-import'./Stays.css';
+import './Stays.css';
+import OfferCard from '../../components/travelOfferCards/offerCard';
 
 function Stays() {
   return (
     <>
-    <Navbar className="navbar" />
-    
-    <div  className='background'>
-      
-    </div>
-    <Footer className="footer"/>
-
+      <Navbar className="navbar" />
+      <div className='background'></div>
+      <div className='offer-card-container'>
+        <OfferCard />
+      </div>
+      <Footer className="footer" />
     </>
-  )
+  );
 }
 
-export default Stays
+export default Stays;
