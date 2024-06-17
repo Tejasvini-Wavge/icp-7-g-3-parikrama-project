8594c0e8c2c87c6aab1a57b784eeb41b734075dc
shriyaborkar358 <shriyaborkar71@gmail.com>
2024-06-17 17:19:44 +0530

    Offer cards added in Flight page

--- src/views/Flights/Flights.js
+++ src/views/Flights/Flights.js
@@ -3,18 +3,35 @@ import './Flights.css'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 import SearchCard from '../../components/searchCard/searchCard'
+import { FlightData } from '../../data/FlightData'
+import FlightOfferCard from '../../components/FlightOfferCard/FlightOfferCard'
 
 
 function Flights() {
   return (
     <>
-    <Navbar/>
-    
-    <div className='background-image'>
-    <SearchCard/>
-    </div>
-    
-    <Footer/>
+      <Navbar />
+
+      <div className='background-image'>
+        <SearchCard />
+      </div>
+
+      <div className='offer-card'>
+        {
+          FlightData.map((dataObject, i) => {
+            const {
+              logo,
+              title,
+              discount,
+              date
+            } =
+              dataObject
+            return (<FlightOfferCard logo={logo} title={title} discount={discount} date={date} />)
+          })
+        }
+      </div>
+
+      <Footer />
     </>
   )
 }
