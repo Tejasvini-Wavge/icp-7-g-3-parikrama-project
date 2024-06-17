f1348256e7567289a36483e8859e89fb197d02b3
shriyaborkar358 <shriyaborkar71@gmail.com>
2024-06-17 16:36:09 +0530

    Header Image added in flight page

---
+++ src/data/FlightData.js
@@ -0,0 +1,8 @@
+const flightData = [
+    {
+        logo: "https://shreethemes.net/geotrip-live/geotrip/assets/img/air-4.png",
+        title: "On Domestic Flight For USA",
+        discount: "30% off",
+        date: "valid 30 june 2024"
+    }
+] 
--- src/views/Flights/Flights.js
+++ src/views/Flights/Flights.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import './Flights.css'
-import HeaderImg from './images/header-flight-img.jpg'
 import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
+import SearchCard from '../../components/searchCard/searchCard'
+
 
 function Flights() {
   return (
     <>
     <Navbar/>
-    <img src={HeaderImg} alt={HeaderImg} className="header-img" />
+    
+    <div className='background-image'>
+    <SearchCard/>
+    </div>
+    
+    <Footer/>
     </>
   )
 }
