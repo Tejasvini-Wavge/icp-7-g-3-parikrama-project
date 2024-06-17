d789bc2d3f9253e4410541199b148683c10fba6c
shriyaborkar358 <shriyaborkar71@gmail.com>
2024-06-18 00:58:29 +0530

    trip days added in location card

--- src/components/LocationCard/LocatinCard.js
+++ src/components/LocationCard/LocatinCard.js
@@ -6,6 +6,7 @@ function LocationCard({ img, location, tripdays, rs }) {
         <div className='location-card'>
         <img src= {img} className='location-image'/>
         <h2 className='location'>{location}</h2>
+        <span className='location'>{tripdays}</span>
         </div>
         </>)
         
