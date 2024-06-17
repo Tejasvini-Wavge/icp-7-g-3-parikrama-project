517f9a306e73bfd9a54d1d922cf4f73f94917d46
shriyaborkar358 <shriyaborkar71@gmail.com>
2024-06-18 00:55:08 +0530

    location added in location card

--- src/components/LocationCard/LocatinCard.js
+++ src/components/LocationCard/LocatinCard.js
@@ -4,7 +4,8 @@ import './LocationCard.css'
 function LocationCard({ img, location, tripdays, rs }) {
     return (<>
         <div className='location-card'>
-        <img src= {img} className='location-img'/>
+        <img src= {img} className='location-image'/>
+        <h2 className='location'>{location}</h2>
         </div>
         </>)
         
