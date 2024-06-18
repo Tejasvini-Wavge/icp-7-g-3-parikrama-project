ef921629f33425a4fdff14bfb1e0673b2517e67b
codewithtarde9370 <teja36491@gmail.com>
2024-06-18 14:55:44 +0530

    adding the changes

--- src/views/Stays/Stays.js
+++ src/views/Stays/Stays.js
@@ -17,6 +17,10 @@ function Stays() {
       <div className='offer-card-container'>
         <OfferCard />
       </div>
+      <div>
+        <p>Why choose us?</p>
+        
+      </div>
       <div className='news-section'>
         <p className='news-heading'>Latest from our news</p>
         <div className='news-container'>
