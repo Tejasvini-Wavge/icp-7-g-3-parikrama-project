63d5ecc669c67b54e1ea0b584745b44260592092
codewithtarde9370 <teja36491@gmail.com>
2024-06-18 08:45:27 +0530

    adding the introduction

--- src/components/introCard/intro.js
+++ src/components/introCard/intro.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import introData from './introData';
-import './intro.css'
+import './intro.css';
 
 function Intro() {
   return (
     <div className='intro-container'>
-        <p className='intro'>{introData}</p>
+      <div className='background-blur'></div>
+      <p className='intro'>"{introData}"</p>
     </div>
-  )
+  );
 }
 
-export default Intro
+export default Intro;
