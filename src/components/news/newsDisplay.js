635907ab6955306d26f628371844eb82236e9e67
codewithtarde9370 <teja36491@gmail.com>
2024-06-18 14:19:07 +0530

    adding the cards

--- src/components/news/newsDisplay.js
+++ src/components/news/newsDisplay.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import NewsData from './newsData';
 import './blogDisplay.css';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
 
 function NewsDisplay() {
   const { id } = useParams();
@@ -11,16 +13,20 @@ function NewsDisplay() {
     return <div>Blog not found!</div>;
   }
 
-  return (
+  return (<>
+<Navbar/>
     <div className="selected-blog">
       <h1>{selectedBlog.title}</h1>
       <div className='data'>
-        <span className='cate'>{selectedBlog.category}</span>
+        <span className='cate'>{selectedBlog.category}</span>|
         <span className='date'>{selectedBlog.date}</span>
       </div>
       <img src={selectedBlog.blogImg} alt={selectedBlog.title} className='blog-image' />
       <p className='content'>{selectedBlog.about}</p>
+
     </div>
+    <Footer/>
+    </>
   );
 }
 
